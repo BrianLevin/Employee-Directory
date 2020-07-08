76c056fbed936a370d3524082a8185ae2f01bebe
Brian Levin <bml201095@gmail.com>
2020-07-08 19:51:54 -0400

    created a function which takes the employee json file then converts from json file to an javascript object and then added the state to it

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,20 +2,34 @@ import React from 'react';
 
 class App extends React.Component {
 
-  state= {
-isLoading: true,
-allEmployees: [],
-employees: []
+  state = {
+    isLoading: true,
+    allEmployees: [],
+    employees: []
 
   }
 
   componentDidMount() {
+    fetch('./employees.json')
+      .then(res => res.json())
+      .then(employees => {
+        this.setState({
+          isLoading: false,
+          employees,
+          allEmployees: employees,
+        });
+
+      });
+
 
 
   }
 
+
+
+
   render() {
-return <div />
+    return <div />
 
   }
 
