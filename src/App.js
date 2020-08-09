7c21a17970d5239f24d450a59cf495f2a54a277e
Brian Levin <bml201095@gmail.com>
2020-08-09 15:26:52 -0400

    changed my App.js file

--- src/App.js
+++ src/App.js
@@ -1,141 +1,20 @@
-import React from 'react';
-import Moment from 'moment';
 
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 
+import "./App.css";
 
-class App extends React.Component {
-
-  state = {
-    isLoading: true,
-    allEmployees: [],
-    employees: []
-
-  }
-
-  componentDidMount() {
-    fetch('./employees.json')
-      .then(res => res.json())
-      .then(employees => {
-        this.setState({
-          isLoading: false,
-          employees,
-          allEmployees: employees,
-        });
-
-      });
-
-  }
-  handleChangePosition = (event) => {
-    const selectedPoisition = event.target.value;
-    const allEmployees = this.state.allEmployees;
-
-    this.setState({
-      employees: allEmployees.filter(employee => {
-        if (employee.position === selectedPoisition) {
-
-          return true;
-        }
-        return false;
-
-      })
-
-
-    });
-  }
-
-    handleSort = (event) => {
-      var sortValue = event.target.value;
-      if (sortValue === 'oldtoYoung') {
-        this.setState({
-          employees: this.state.employees.sort((employee1, employee2) => {
-            if (Moment(employee1.DOB) < Moment(employee2.DOB)) {
-              return -1;
-            }
-            if (Moment(employee1.DOB) > Moment(employee2.DOB)) {
-              return 1;
-
-            }
-            return 0;
-          })
-
-        })
-      }
-      if (sortValue === 'youngToOld') {
-        this.setState({
-          employees: this.state.employees.sort((employee1, employee2) => {
-            if (Moment(employee1.DOB) > Moment(employee2.DOB)) {
-              return -1;
-            }
-            if (Moment(employee1.DOB) < Moment(employee2.DOB)) {
-              return 1;
-
-            }
-            return 0;
-          })
-
-        })
-      }
-    }
-  
-
-  render() {
-    if (this.state.isLoading) {
-      return <div> Loading...</div>
-
-    }
-    return (
-      <>
-        <div>
-          <label> Filter By Position</label>
-          <select onChange={this.handleChangePosition}>
-            <option value="All">All</option>
-            <option value="manager">manager</option>
-            <option value="front-end">fonr-end</option>
-            <option value="back-end">back-end</option>
-            <option value="janitor">janitor</option>
-            <option value="marketing ">marketing</option>
-            <option value="cashier">cashier</option>
-            <option value="driver">driver</option>
-            <option value="engineer">engineer</option>
-            <option value="accountant">accountant</option>
-
-            <option value="manager">Manager</option>
-          </select>
-
-
-        </div>
-        <label>Sort by</label>
-        <select onChange={this.handleSort}>
-          <option />
-          <option value="youngtoOld"> Youngest to Oldest</option>
-          <option value="oldtoYoung"> Oldest to Youngest</option>
-
-
-        </select>
-
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Email></th>
-            <th>Position</th>
-            <th>Date of Birth</th>
-          </tr>
-          {this.state.employees.map(employee => (
-            <tr key={employee.id}>
-              <td>{employee.name}</td>
-              <td>{employee.email}</td>
-              <td>{employee.DOB}</td>
-              <td>{employee.position}</td>
-
-            </tr>
-          ))}
-        </table>
-
-      </>
-    )
-
-  }
-
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
